e5654a40a046feba6ae2155fa102748f53796a0d
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2026-03-31 21:13:41 +0200

    simplify asset bundle

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: false, // single js file
+      },
+    },
+  },
   plugins: [react()],
 })
